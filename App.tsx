690a109effa5be4fd93d8c373afb5f1c82f574a4
luanmxz <luanmarcenemxz22@gmail.com>
2023-01-09 21:54:47 -0300

    Styled Components config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Text } from './src/components/Button';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<Container>
+			<Text>Olimpo</Text>
+		</Container>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
