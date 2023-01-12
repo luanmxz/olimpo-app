4e62dea42e9e586159a43b72dfe3b63eabe95d30
luanmxz <luanmarcenemxz22@gmail.com>
2023-01-11 22:10:55 -0300

    ADD: React navigation and first configuration

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home/Home';
 
 import dark from './src/theme/dark';
 import light from './src/theme/light';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
 	return (
 		<ThemeProvider theme={dark}>
-			<Home />
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name='Home' component={Home} />
+				</Stack.Navigator>
+			</NavigationContainer>
 		</ThemeProvider>
 	);
 }
